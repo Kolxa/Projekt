985b79035172f7dd95cfbcb7eab339f66e2fed14
Ivan <rety123ty@gmail.com>
2023-01-31 21:39:11 +0100

    asas

--- kod/fun.h
+++ kod/fun.h
@@ -2,6 +2,7 @@
 #define FUN_H_INCLUDED
 #include "add.cpp"
 #include "odej.cpp"
+
 int add(int x,int y);
 int odej(int x,int y);
 
--- kod/odej.h
+++
@@ -1,8 +0,0 @@
-#ifndef ODEJ_H_INCLUDED
-#define ODEJ_H_INCLUDED
-#include "odej.cpp"
-
-int odej(int x,int y);
-
-
-#endif // ODEJ_H_INCLUDED
